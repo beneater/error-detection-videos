680862eca82d7a9808c7640cc0263b2a9a1b8e25
Ben Eater <eater@khanacademy.org>
2018-06-09 11:58:54 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Error detection video series
+
+Copyright 2018 Ben Eater
+
+This code is [MIT licensed](http://en.wikipedia.org/wiki/MIT_License).
+
+## What is this?
+
+This repo contains the code to accompany a YouTube mini-series on reliable communication, error detection, and correction. To have any idea what's going on here, you should watch the videos:
+
+
+## Reliable data transmission
+
+[<img src=https://img.youtube.com/vi/eq5YpKHXJDM/maxresdefault.jpg width=360/>](https://www.youtube.com/watch?v=eq5YpKHXJDM)
+
+This is the first video in the series and covers mostly setup for the future videos. In the video we build a simple transmitter and receiver:
+
+ * [`/transmitter`](/transmitter)
+ * [`/receiver`](/receiver)
+
+
+## More?
+
+The rest of the videos are coming soon!
---
+++ receiver/receiver.ino
@@ -0,0 +1,66 @@
+#include <LiquidCrystal.h>
+
+// Pin assignments
+#define RX_CLOCK 2
+#define RX_DATA 3
+#define LCD_D4 4
+#define LCD_D5 5
+#define LCD_D6 6
+#define LCD_D7 7
+#define LCD_RS 8
+#define LCD_EN 9
+
+LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
+
+char message[16];
+volatile byte rx_byte = 0;
+volatile int bit_position = 0;
+volatile bool update_lcd = true;
+
+void setup() {
+  pinMode(RX_DATA, INPUT);
+  strcpy(message, "");
+  lcd.begin(16, 2);
+  attachInterrupt(digitalPinToInterrupt(RX_CLOCK), onClockPulse, RISING);
+}
+
+void onClockPulse() {
+  bool rx_bit = digitalRead(RX_DATA);
+
+  if (bit_position == 8) {
+    rx_byte = 0;
+    bit_position = 0;
+  }
+  
+  if (rx_bit) {
+    rx_byte |= (0x80 >> bit_position);
+  }
+
+  bit_position += 1;
+
+  if (bit_position == 8) {
+    strncat(message, &rx_byte, 1);
+  }
+  
+  update_lcd = true;
+}
+
+void loop() {
+  if (update_lcd) {
+    update_lcd = false;
+    
+    lcd.noCursor();
+    lcd.setCursor(0, 0);
+    lcd.print(message);
+    lcd.setCursor(0, 1);
+    for (int i = 0; i < 8; i += 1) {
+      if (i < bit_position) {
+        lcd.print(rx_byte & (0x80 >> i) ? "1" : "0");
+      } else {
+        lcd.print(" ");
+      }
+    }
+    lcd.setCursor(strlen(message), 0);
+    lcd.cursor();
+  }
+}
